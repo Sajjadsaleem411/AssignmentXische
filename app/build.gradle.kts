a66deabbe1db6cd08ba11dbd611244889aa85fc5
Muhammad Sajjad <muh.sajjadsaleem@gmail.com>
2024-06-01 22:04:46 +0500

    add navigation lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,4 +81,9 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
 }
